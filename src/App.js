de825a34495fc167e4f64194b78bb4655c5088d3
Amrit Kumar Mohanty <amritmohanty60@gmail.com>
2023-07-27 11:15:03 +0530

    fix - textcontent correction in output area

--- src/App.js
+++ src/App.js
@@ -2,13 +2,26 @@ var inputTxt = document.querySelector("#input--txt");
 var btnTranslate = document.querySelector("#btn--translate");
 var outputTxt = document.querySelector("#output--txt");
 
+var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
+
+var temp = "";
+
+function fetchTxt(url){
+    var queryURL = url+"?text="+inputTxt.value;
+    fetch(queryURL).then(res => res.json()).then(json => {
+        outputTxt.innerHTML = json.contents.text
+        console.log(outputTxt.value)
+})
+}
+
+
 
 var clickHandler = function handleClick(){
     console.log("click detected")
     console.log(inputTxt.value)
-    outputTxt.innerHTML = inputTxt.value
-    console.log(outputTxt.value)
+    fetchTxt(serverURL)
 }
+
 
 
 
