411bae841a1a44ed35662486953372f8413c4560
Amrit Kumar Mohanty <amritmohanty60@gmail.com>
2023-07-27 11:32:40 +0530

    Chore: replaced dummy api with real  api

--- src/App.js
+++ src/App.js
@@ -2,23 +2,23 @@ var inputTxt = document.querySelector("#input--txt");
 var btnTranslate = document.querySelector("#btn--translate");
 var outputTxt = document.querySelector("#output--txt");
 
-var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
-
-var temp = "";
+var serverURL = "https://api.funtranslations.com/translate/morse.json"
 
 function fetchTxt(url){
     var queryURL = url+"?text="+inputTxt.value;
-    fetch(queryURL).then(res => res.json()).then(json => {
-        outputTxt.innerHTML = json.contents.text
-        console.log(outputTxt.value)
-})
+    fetch(queryURL)
+    .then(res => res.json())
+    .then(json => {
+        outputTxt.innerHTML = json.contents.translated
+    }).catch(error => {
+        alert(error)
+        console.log(error)
+    })
 }
 
-
-
 var clickHandler = function handleClick(){
-    console.log("click detected")
-    console.log(inputTxt.value)
+    // console.log("click detected")
+    // console.log(inputTxt.value)
     fetchTxt(serverURL)
 }
 
